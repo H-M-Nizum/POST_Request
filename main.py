6f919f601a36a4a1d302c48c7f909c34ace5f81e
H.M.Nizum <hmnizum1714032@gmail.com>
2024-05-20 16:01:01 +0600

    get data and post this data in a specific url

---
+++ main.py
@@ -0,0 +1,44 @@
+from flask import Flask, request, jsonify, render_template
+from flask_cors import CORS
+import requests
+import json
+
+app = Flask(__name__)
+CORS(app) 
+@app.route('/receive-data', methods=['POST'])
+def receive_data():
+    data = request.json  
+   
+    print('Received data:', data)
+    
+    # Post the received data to another URL
+    post_data_to_another_url(data)
+
+
+    return data
+
+def post_data_to_another_url(data):
+    print(data)
+    
+    # Define the URL to post the data to
+    url = 'https://demo15.ionicerp.xyz/api/resource/benner'
+
+    try:
+        headers = {
+            "Accept": "application/json",
+            "Content-Type": "application/json",
+            "Authorization" : "token 7b052228a6fd29d:7f323743adf2694"
+        }
+    #     # Post the data to the other URL
+    #     # response = requests.post(another_url, json=data)
+        response = requests.post(url, json=data, headers=headers)
+        response.raise_for_status()  # Raise an exception for 4xx or 5xx status codes
+        print('Data posted to another URL successfully')
+        print(response)
+    except requests.exceptions.RequestException as e:
+        print('Error posting data to another URL:', e)
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)  # Run Flask app
