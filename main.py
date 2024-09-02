87c6e1ec32cfa2354fe6ff985dbff386cd2982cf
H.M.Nizum <hmnizum1714032@gmail.com>
2024-09-02 18:28:42 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
+import os
 
 # Sample data
 import requests
@@ -53,4 +54,5 @@ def get_all_documents():
     return jsonify(data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
