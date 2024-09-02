6fc46245fbf809d0f720db6127318f3a72286160
H.M.Nizum <hmnizum1714032@gmail.com>
2024-09-02 18:07:50 +0600

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,56 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, jsonify, request
 from flask_cors import CORS
+
+# Sample data
 import requests
-import json
+
+def fetch_data(param1):
+    url = f"""https://erp.gawsiashop.com.bd/api/resource/Item?filters=[["item_group", "=", "{param1}"]]&fields=["*"]&limit_page_length=99"""
+    headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Authorization" : "token 092867a6814bfc7:bcc0df87d4b1a77"
+    }
+    payload = {
+        "param1": param1
+    }
+    response = requests.get(url, headers=headers, params=payload)
+    return response.json()
+
 
 app = Flask(__name__)
 CORS(app) 
-@app.route('/receive-data', methods=['POST'])
-def receive_data():
-    data = request.json  
-   
-    print('Received data:', data)
+@app.route('/get', methods=['GET'])
+def get_document():
+    param1 = request.args.get('param1')  
+    # Accessing param2 from query string
+    if param1 is None:
+        return jsonify({"error": "Missing parameters"}), 400
+    data = fetch_data(param1)
+    if 'data' not in data or not data['data']:
+        return jsonify({"error": "No data found"}), 404
+    print(len(data["data"][0]), 'bal')
+    return jsonify(data)
+
+
+#----------------------------------------------------------------------------------------------------------------------------
+def fetch_all_data():
+    url = f"""https://erp.gawsiashop.com.bd/api/resource/Item Group?fields=["*"]&limit_page_length=99"""
+    headers = {
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+        "Authorization" : "token 092867a6814bfc7:bcc0df87d4b1a77"
+    }
+    # Assuming you want to use param1 and param2 in the request
+    response = requests.get(url, headers=headers)
+    return response.json()
+
+@app.route('/getall', methods=['GET'])
+def get_all_documents():
     
-    # Post the received data to another URL
-    post_data_to_another_url(data)
-
-
-    return data
-
-def post_data_to_another_url(data):
-    print(data)
-    
-    # Define the URL to post the data to
-    url = f"https://{data['server']}.ionicerp.xyz/api/resource/{data['doctype']}"
-
-    try:
-        headers = {
-            "Accept": "application/json",
-            "Content-Type": "application/json",
-            "Authorization" : "token 7b052228a6fd29d:7f323743adf2694"
-        }
-    #     # Post the data to the other URL
-    #     # response = requests.post(another_url, json=data)
-        response = requests.post(url, json=data, headers=headers)
-        response.raise_for_status() 
-        print('Data posted to another URL successfully')
-        print(response)
-    except requests.exceptions.RequestException as e:
-        print('Error posting data to another URL:', e)
-
-
+    data = fetch_all_data()
+    print(len(data["data"][0]))
+    return jsonify(data)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')  
+    app.run(debug=True)
